0f42bbebc1b9a5980b4a976ebfb3c1d052413c5a
Jowcode Software <jhonatan.elvis1995@gmail.com>
2025-02-01 01:20:28 +0000

    "commit para react"

---
+++ flamework-app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+const url = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/798.png"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>titulo</h1>
+      <p App>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Architecto magnam debitis soluta minima amet necessitatibus minus obcaecati 
+        quisquam in accusantium repellendus ipsa consequatur omnis fugiat provident nemo ad, sequi iusto.
+      </p>
+      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/798.png"></img>
+
+    </div>
+  );
+}
+
+export default App;
